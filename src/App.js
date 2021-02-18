a7ae7b916bd333c91962126ab2eff3dec7bb13f1
Alan Marin <alan.marin@mail.escuelaing.edu.co>
2021-02-18 14:40:52 -0500

    Update

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React,{Component} from 'react'
+import logo from './logo.svg';
+import {Login} from './components/Login';
+import {TodoApp} from './components/TodoApp';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+const LoginView = () => (
+    <Login/>
+);
+
+const TodoAppView = () => (
+    <TodoApp/>
+);
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state ={isLoggedIn : false};
+    }
+
+    
+
+    render() {
+
+        return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
+
+                    <br/>
+                    <br/>
+
+                    <ul>
+                        <li><Link to="/">Login</Link></li>
+                        <li><Link to="/todo">Todo</Link></li>
+                    </ul>
+
+                    <div>
+                        <Route exact path="/" component={LoginView}/>
+                        <Route path="/todo" component={TodoAppView}/>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+
+}
+
+export default App;
